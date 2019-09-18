3e9549cb0d2925e6cc011d0133be2b4d7b52a4ec
Scott Lawson <scottie@scotts-air.home>
2019-09-18 07:48:23 -0700

    starting to come along. reads filename and file length from connection.

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,7 +8,7 @@ use std::string;
 #[derive(Debug)]
 enum TransfrProtocolError {
     IoError(io::Error),
-    Utf8Error(string::FromUtf8Error),    
+    Utf8Error(string::FromUtf8Error),
 }
 
 impl fmt::Display for TransfrProtocolError {
@@ -44,39 +44,50 @@ fn new_connection(stream: TcpStream) {
 }
 
 fn handle_connection(stream: TcpStream) {
-    let _filename = match read_file_name(stream) {
-        Ok(f) => f,
+    let mut _filename = "".to_string();
+    match read_file_name(&stream) {
+        Ok(f) => _filename = f,
+        Err(e) => println!("Error reading filename: {}!", e),
+    };
+
+    let mut _file_length = 0 as u64;
+    match read_file_length(&stream) {
+        Ok(len) => _file_length = len,
         Err(e) => {
-            println!("Error reading filename: {}!", e);
-            return //closes the connection per documentation
+            println!("Error reading file length: {}!", e);
         }
     };
 }
 
-fn read_file_name(mut stream: TcpStream) -> Result<String, TransfrProtocolError> {
-
+fn read_file_name(mut stream: &TcpStream) -> Result<String, TransfrProtocolError> {
     let mut buf = [0 as u8; 8];
     stream.read_exact(&mut buf)?;
 
-    let mut buf = vec![0 as u8; to_i64_be(buf)];
-    stream.read_exact(&mut buf)?; 
+    let mut buf = vec![0 as u8; to_u64_be(buf) as usize];
+    stream.read_exact(&mut buf)?;
 
     //pub fn from_utf8(vec: Vec<u8>) -> Result<String, FromUtf8Error>
     //@TODO i need to figure out why this compiles. To clarify: IMO it should compile without
     //the Ok(filename) part.
-    return filename = String::from_utf8(buf)?;
+    let filename = String::from_utf8(buf)?;
+    Ok(filename)
 }
 
-fn to_i64_be(bytes: [u8; 8]) -> usize {
-    let mut val: usize = 0;
-    val += (bytes[7] as usize) >> 56;
-    val += (bytes[6] as usize) >> 48;
-    val += (bytes[5] as usize) >> 40;
-    val += (bytes[4] as usize) >> 32;
-    val += (bytes[3] as usize) >> 24;
-    val += (bytes[2] as usize) >> 16;
-    val += (bytes[1] as usize) >> 8;
-    val += (bytes[0] as usize) >> 0;
+fn read_file_length(mut stream: &TcpStream) -> Result<u64, TransfrProtocolError> {
+    let mut buf = [0 as u8; 8];
+    stream.read_exact(&mut buf)?;
+    Ok(to_u64_be(buf))
+}
 
+fn to_u64_be(bytes: [u8; 8]) -> u64 {
+    let mut val: u64 = 0;
+    val += (bytes[7] as u64) >> 56;
+    val += (bytes[6] as u64) >> 48;
+    val += (bytes[5] as u64) >> 40;
+    val += (bytes[4] as u64) >> 32;
+    val += (bytes[3] as u64) >> 24;
+    val += (bytes[2] as u64) >> 16;
+    val += (bytes[1] as u64) >> 8;
+    val += (bytes[0] as u64) >> 0;
     return val;
 }
